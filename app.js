98239fd5c0b3457d61326bbf26bfa5727c04336b
sadath <sadathshariff65@gmail.com>
2020-12-08 13:24:25 +0530

    input output done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hi from script");
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("textarea");
+var outputText = document.querySelector("#outputText");
+
+btnTranslate.addEventListener("click",function translating(){
+    console.log("clicked");
+    console.log("input :",inputText.value);
+    console.log("output :" + outputText.innerHTML);
+});
+
+translating();
